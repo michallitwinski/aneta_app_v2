895e228a4712351621694b4a060fa747111ff05a
michallitwinski <m.m.litwinscy@gmail.com>
2022-09-07 05:29:26 +0200

    Animacja v2

--- public/javasripts/script.js
+++ public/javasripts/script.js
@@ -3,12 +3,18 @@ const slider_dwa = document.querySelector('.slider-dwa');
 const slider_trzy = document.querySelector('.slider-trzy');
 const slider_cztery = document.querySelector('.slider-cztery');
 const slider_piec = document.querySelector('.slider-piec');
+const slider_napis = document.querySelector('.slider-napis');
+const div_opis = document.querySelector('.div-opis');
 function start(){
-    alert('Ok');
     slider_raz.classList.add('slider-raz-anim');
     slider_dwa.classList.add('slider-dwa-anim');
     slider_trzy.classList.add('slider-trzy-anim');
     slider_cztery.classList.add('slider-cztery-anim');
     slider_piec.classList.add('slider-piec-anim');
+    slider_napis.classList.remove('slider-napis-start');
+    slider_napis.classList.add('slider-napis-const');
+    div_opis.classList.remove('display-none');
+    div_opis.classList.add('opacity-start');
+
 };
 setTimeout(start, 5000);
